4128b68437275ca6b5e97f3ac1c1f48a5fddb226
unknown <valeryr89@gmail.com>
2023-05-28 21:48:06 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SampComponent } from './samp/samp.component';
 import { HttpClientModule } from '@angular/common/http';
+import { Comp1Component } from './components/comp1/comp1.component';
+import { Comp2Component } from './components/comp2/comp2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SampComponent
+    SampComponent,
+    Comp1Component,
+    Comp2Component
   ],
   imports: [
     BrowserModule,
